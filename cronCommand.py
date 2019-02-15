5cc84538df30be0f7ac3b382e4a04b6437c14b57
gza060625 <gza060625@gmail.com>
2019-02-15 10:01:01 -0800

    working

---
+++ cronCommand.py
@@ -0,0 +1,3 @@
+57 * * * * python3 /home/gza060625/Desktop/AU/logSatelliteWeather/smallFunctions.py 
+#51 * * * * python3 smallFunctions.py 
+#52 * * * * python3 smallFunctions.py 
--- smallFunctions.py
+++ smallFunctions.py
@@ -6,30 +6,34 @@ import datetime
 import wget
 
 
-#Constant Data
+'''
+Test Command
+python3 '/home/gza060625/Desktop/AU/logSatelliteWeather/SATWEATHER' "GOES" "https://weather.gc.ca/data/satellite/goes_wcan_visible_100.jpg"
+'''
 
-timeInterval=2
+
+
+#Constant Data
 
 configList=[('/home/gza060625/Desktop/AU/logSatelliteWeather/SATWEATHER',\
              "GOES",\
-             "https://weather.gc.ca/data/satellite/goes_wcan_visible_100.jpg")\
-            ]
+             
+             
+             
+             
+             "https://weather.gc.ca/data/satellite/goes_wcan_visible_100.jpg"\
+            ),\
+            ('/home/gza060625/Desktop/AU/logSatelliteWeather/SATWEATHER',\
+             "AUGO",\
+             "http://www.cleardarksky.com/c/AUGOABcsk.gif"\
+            ),\
+           ]
 
-
-
-'''
-
-Adding Star before print, then start a new line
-
-If input is integer, then print stars accordingly.
-
-'''
 
 
 '''
 generate file name
 '''
-
 def createFileName(url):
     return UTimeStr()+getFileType(url)
 
@@ -43,8 +47,9 @@ def createFolder(outputPath,siteName,year, month,day):
     year=str(year)
     month=str(month)
     day=str(day)
+    
     path=os.path.join(outputPath,siteName,year,month,day)
-    print(path)
+    #print(path)
     os.makedirs(path, exist_ok=True) 
     return path
 
@@ -62,7 +67,6 @@ Return Current UT time as yead,month,year
 def UTime():
     now=datetime.datetime.now()
     return now.year,now.month,now.day
-
 
 
 def pullInfo(url,filePath):     
@@ -86,20 +90,16 @@ def main():
     
     global configList
     
-    if len(sys.argv)>1:
-        print(sys.argv)
-    else:
-
+    if len(sys.argv)==1:
         for ele in configList:
             fetchFile(*ele)
-
+        
+    elif len(sys.argv)==4:
+        fetchFile(*sys.argv[1:4])
+    else:
+        print("3 arguments required, {} provided.".format(len(sys.argv)-1))
+        print(sys.argv)  
         os._exit(0)
-
-
-
-
-
-
 
 
 
